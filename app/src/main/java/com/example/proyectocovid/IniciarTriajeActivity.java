7201a7295be16e7aa40b300ea5182035ab4d0714
Hellen <hellentrejo@gmail.com>
2020-05-02 16:18:15 -0500

    Agregando interfaces a la Rama triaje -Hellen Trejo

---
+++ app/src/main/java/com/example/proyectocovid/IniciarTriajeActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectocovid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IniciarTriajeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_triaje);
+    }
+}
