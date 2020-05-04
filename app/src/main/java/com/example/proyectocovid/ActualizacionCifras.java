7d5f0bca8066cef61733d978ead8e4c5dbdde91e
JesusMZarate <jesusmzarate96status@gmail.com>
2020-05-03 19:13:10 -0500

    Agregado el actualizar cifras

---
+++ app/src/main/java/com/example/proyectocovid/ActualizacionCifras.java
@@ -0,0 +1,14 @@
+package com.example.proyectocovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActualizacionCifras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizacion_cifras);
+    }
+}
