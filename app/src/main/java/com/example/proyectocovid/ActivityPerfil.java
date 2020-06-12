cb8bece15e39097ed6346c57addcae3c58c31529
Hellen <hellentrejo@gmail.com>
2020-06-11 23:02:21 -0500

    Añadiendo detalles

--- app/src/main/java/com/example/proyectocovid/ActivityPerfil.java
+++ app/src/main/java/com/example/proyectocovid/ActivityPerfil.java
@@ -1,15 +1,24 @@
 package com.example.proyectocovid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityPerfil extends AppCompatActivity {
+    Spinner mySpinner;
   ImageView btnTriajePerfil;
   TextView txtNumDocPerfil , txtNumCelPerfil, txtNacionPerfil;
 
@@ -18,6 +27,28 @@ public class ActivityPerfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        Toolbar toolbar = findViewById(R.id.toolbar_sup);
+        setSupportActionBar(toolbar);
+        mySpinner =(Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter =  new ArrayAdapter<String>(ActivityPerfil.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.opciones_persona));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         txtNumDocPerfil=(TextView)findViewById(R.id.tvNumDocPerfil);
         txtNacionPerfil=(TextView)findViewById(R.id.txNacionPerfil);
         txtNumCelPerfil=(TextView)findViewById(R.id.tvNumCelPerfil);
@@ -45,4 +76,26 @@ public class ActivityPerfil extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_sup,menu);
+        return super.onCreateOptionsMenu(menu);
+        //return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_cerrar:
+                Toast.makeText(this, "Cerró sesión",Toast.LENGTH_SHORT).show();
+                Intent myIntent = new Intent(ActivityPerfil.this,MainActivity.class);
+                startActivityForResult(myIntent,0);
+                return true;
+            //break;
+        }
+        return super.onOptionsItemSelected(item);
+        //return true;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/proyectocovid/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.proyectocovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logosplashCo;
+    private static int splashTimeOut=5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logosplashCo=(ImageView) findViewById(R.id.logoSplash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim= AnimationUtils.loadAnimation(this,R.anim.mysplash);
+        logosplashCo.startAnimation(myanim);
+    }
+}
