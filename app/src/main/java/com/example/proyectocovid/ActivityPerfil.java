9e780433730572b44fe67bedb4f264a95aac3a0e
Hellen <hellentrejo@gmail.com>
2020-06-12 22:23:54 -0500

    Modificando

--- app/src/main/java/com/example/proyectocovid/ActivityPerfil.java
+++ app/src/main/java/com/example/proyectocovid/ActivityPerfil.java
@@ -18,7 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityPerfil extends AppCompatActivity {
-    Spinner mySpinner;
+
   ImageView btnTriajePerfil;
   TextView txtNumDocPerfil , txtNumCelPerfil, txtNacionPerfil;
 
@@ -29,24 +29,9 @@ public class ActivityPerfil extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
         Toolbar toolbar = findViewById(R.id.toolbar_sup);
         setSupportActionBar(toolbar);
-        mySpinner =(Spinner) findViewById(R.id.spinner);
-
-        ArrayAdapter<String> myAdapter =  new ArrayAdapter<String>(ActivityPerfil.this,android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.opciones_persona));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
 
 
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
 
 
         txtNumDocPerfil=(TextView)findViewById(R.id.tvNumDocPerfil);
@@ -90,6 +75,18 @@ public class ActivityPerfil extends AppCompatActivity {
                 Toast.makeText(this, "Cerró sesión",Toast.LENGTH_SHORT).show();
                 Intent myIntent = new Intent(ActivityPerfil.this,MainActivity.class);
                 startActivityForResult(myIntent,0);
+                return true;
+            //break;
+            case R.id.menu_perfil:
+                //Toast.makeText(this, "Cerró sesión",Toast.LENGTH_SHORT).show();
+                //Intent myp = new Intent(ActivityPerfil.this,ActivityPerfil.class);
+                //startActivityForResult(myp,0);
+                return true;
+            //break;
+            case R.id.menu_cifras:
+                //Toast.makeText(this, "Cerró sesión",Toast.LENGTH_SHORT).show();
+                Intent myc = new Intent(ActivityPerfil.this,BuenaRespuestaActivity.class);//Cambiar
+                startActivityForResult(myc,0);
                 return true;
             //break;
         }
